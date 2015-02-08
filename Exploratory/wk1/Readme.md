f8a9af3f16b22f71e7cbca1eb7f7f901cc141b75
JorisSchut <jorisschut@hotmail.nl>
2015-02-08 12:16:10 +0100

    Exploratory data analysis project 1
    Files from the first Exploratory data analysis assignment.

---
+++ Exploratory/wk1/Readme.md
@@ -0,0 +1,19 @@
+---
+title: "Readme"
+author: "Joris Schut"
+date: "Sunday, February 08, 2015"
+output: html_document
+---
+
+#Exploratory data analysis assignment 1
+
+The files in this directory  print the 4 plots required for the 1st assignment of
+the eploratory data analysis course on Coursera. It takes a selection of the
+"Individual household electric power consumption Data Set" from the UCI Machine Learning
+Repository to create 4 plots.
+
+## R code
+The code used to create the subset of data used here is the same accross the 4 scripts. As it can take some time to run it is advised to only run the code once.
+
+##Data
+The data used to create the plots is available in this folder or at the UCI Machine Learning Repository at: https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
---
+++ Exploratory/wk1/plot2.R
@@ -0,0 +1,29 @@
+# Load in the required libraries
+library(dplyr)
+library(lubridate)
+
+#Reads the dataset into R and converts it in a tbl_df
+power <- read.csv("household_power_consumption.txt", sep = ";", 
+                   col.names = c("Date", "Time", "Global_active_power",
+                                 "Global_reactive_power", "Voltage", "Global_intensity",
+                                 "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+                   na.strings = "?", 
+                   colClasses = c("character", "character", "numeric", "numeric",
+                                   "numeric", "numeric", "numeric", "numeric",
+                                  "numeric"),
+                  nrows = 2075259)
+power<- tbl_df(power)
+
+#Mutates the date and time variables and converts them into the POSIXct class
+power <- mutate(power, dtime = paste(Date, Time))
+power$Date <- as.Date(power$Date, format = "%d/%m/%Y")
+power$dtime <- as.POSIXct(power$dtime, format = "%d/%m/%Y %H:%M:%S")
+power <- filter(power, Date > '2007-01-31' & Date < '2007-02-03')
+
+#Plots the graph
+plot(power$dtime, power$Global_active_power, type = "l", 
+     xlab = "", ylab = "Global Active Power")
+
+#Writes the plot as a png file
+dev.print(png, file = "plot2.png", width = 480, height = 480)
+dev.off
